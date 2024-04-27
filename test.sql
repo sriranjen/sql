0d2260632fb8236d3c8c0f7ca73c407e0a306604
sriranjen <111434246+sriranjen@users.noreply.github.com>
2024-04-27 22:09:43 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,6 +1,6 @@
 -- Task 1: Retrieve the names of all books in the library
 SELECT title FROM books;
--- Answer: [List of book titles]
+
 -- Task 2: Find the titles of all books published after 2010
 SELECT title FROM books WHERE publication_year > 2010;
 -- Answer: [List of book titles]
