508c5eb37a22813619b41ea22f4ae231bf5f7912
sriranjen <111434246+sriranjen@users.noreply.github.com>
2024-04-30 20:35:46 +0530

    Update QUICK_SORT.java

--- QUICK_SORT.java
+++ QUICK_SORT.java
@@ -1,3 +1,4 @@
+// increase order
 public class QuickSort {
     public static void main(String[] args) {
         int[] arr = {5, 2, 9, 1, 5, 6};
@@ -28,6 +29,46 @@ public class QuickSort {
             }
         }
         int temp = arr[i + 1];
+
+        // decerease order
+        public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        quick(arr, 0, arr.length - 1);
+        System.out.println("Sorted array in decreasing order:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            // Compare elements to pivot for decreasing order
+            if (arr[j] > pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void quick(int arr[], int low, int high) {
+        if (low < high) {
+            int pivotindex = partition(arr, low, high);
+            // Swap the positions of recursive calls for decreasing order
+            quick(arr, low, pivotindex - 1);
+            quick(arr, pivotindex + 1, high);
+        }
+    }
+}
+
         arr[i + 1] = arr[high];
         arr[high] = temp;
         return i + 1;
